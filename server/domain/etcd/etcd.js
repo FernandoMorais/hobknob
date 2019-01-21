54083c1e509e14cd7720c8d505faf9e748e833af
Fernando de Morais <mnando@gmail.com>
2019-01-21 18:25:40 -0200

    Fix etcd client configuration.

--- server/domain/etcd/etcd.js
+++ server/domain/etcd/etcd.js
@@ -3,4 +3,4 @@
 const config = require('config');
 const Etcd = require('node-etcd');
 
-module.exports.client = new Etcd(config.etcdHost, config.etcdPort);
+module.exports.client = new Etcd(`${config.etcdHost}:${config.etcdPort}`);
